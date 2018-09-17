ebc8f6214dc44dfcdaf8aabf83c37f3b075b1e74
Ryan Cockram <ryan.cockram@ecutbildning.se>
2018-09-17 13:26:29 +0200

    created basic properties for a person

--- LessthanThree/LessthanThree/Domain/Person.cs
+++ LessthanThree/LessthanThree/Domain/Person.cs
@@ -3,9 +3,30 @@ namespace LessthanThree.Domain
 {
     public class Person
     {
-        public Person()
-        {
+        public string FirstName     { get; set; }
+        public string LastName      { get; set; }
+        public int Age              { get; set; }
+        public char Gender          { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool StatusSingle    { get; set; }
+        public bool Smoker          { get; set; }
+        public bool HasPets         { get; set; }
+        public bool HasJob          { get; set; }
+        public bool HasEducation    { get; set; }
 
+        public Person(string firstName, string lastName, int age, char gender, DateTime dob,
+                     bool statusSingle, bool smoker, bool hasPets, bool hasJob, bool hasEducation)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Gender = gender;
+            DateOfBirth = dob;
+            StatusSingle = statusSingle;
+            Smoker = smoker;
+            HasPets = hasPets;
+            HasJob = hasJob;
+            HasEducation = hasEducation;
         }
     }
 }
