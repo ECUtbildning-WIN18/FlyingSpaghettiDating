a171dbbfe5b289297a1bdac5db58a04cb43d2f09
Anders Pamp <anders_pamp@hotmail.com>
2018-09-19 14:11:15 +0200

    From scratch

---
+++ LessthanThree/Domain/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LessthanThree.Domain
+{
+    class Menu
+    {
+        public void MainMenu()
+        {
+            Console.WriteLine("Login - press '1'");
+            Console.WriteLine("Join - press '2'");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    {
+                        Profile.
+                    }
+            }
+        }
+       
+    }
+}
---
+++ LessthanThree/Domain/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LessthanThree.Domain
+{
+    class Profile
+    {
+        public void ProfileMenu()
+        {
+
+        }
+    }
+}
